e95c842ebcc8368872d7012db016e9b04077dc73
S1xRC <163365038+S1xRC@users.noreply.github.com>
2024-03-14 14:00:10 +0000

    ola

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,26 @@
+import random
+import time
+
+
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+    puntaje = 0
+    print("Debes memorizar 10 números entre el 1 y 10 en 10 segundos.\n")
+    adivinar = []
+    asd = ""
+    for x in range(10):
+        numero = random.randint(1, 10)
+        adivinar.append(str(numero))
+    asd = " ".join(adivinar)
+    print("¡Memoriza! \n\n" + asd)
+    time.sleep(10)
+    print("#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n#\n")
+
+    print("¡Adivina!\n")
+
+    for i in range(10):
+        intento = input(f"{i + 1}) ")
+        if intento == adivinar[i]:
+            puntaje += 1
+    print(f"¡Obtuviste {puntaje} puntos!\n\n Gracias por jugar :D")
+
+memoria()
