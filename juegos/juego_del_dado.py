783f7b30ed296c857f9c9e3867708b8e7ecad733
S1xRC <163365038+S1xRC@users.noreply.github.com>
2024-03-14 15:12:42 +0000

    elou

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,8 +1,35 @@
+import random
+import time
+
+
 def juego_del_dado():
-    """
-    Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
-    Esto genera un número al azar que se le suma a la puntuación del usuario.
-    Después el computador también tiene que lanzar un dado.
-    El primero en sumar 30 puntos gana.
-    """
-    pass
+    print("\n\nEn este juego compites contra la computadora, el primero en llegar a 30 lanzando un dado de 6 caras gana.\n")
+    puntaje_jugador = 0
+    puntaje_computadora = 0
+    while True:
+        print("Turno del jugador:\n")
+        puntaje = input("¡Lanza el dado!\n\n")
+        time.sleep(0.5)
+        puntaje = random.randint(1, 6)
+        print(f"\n¡Obtuviste {puntaje} puntos!\n")
+        time.sleep(0.5)
+        puntaje_jugador += puntaje
+        if puntaje_jugador >= 30:
+            print("¡El jugador gano! :DD")
+            break
+        time.sleep(0.5)
+        print("Turno de la computadora:\n")
+        time.sleep(0.5)
+        puntaje = random.randint(1, 6)
+        print(f"La computadora lanzo y obtuvo {puntaje} puntos\n")
+        time.sleep(0.5)
+        puntaje_computadora += puntaje
+        if puntaje_computadora >= 30:
+            print("La computadora gano :(((")
+            break
+        print(
+            f"Puntajes:\n\n| Jugador: {puntaje_jugador} | Computadora: {puntaje_computadora} |\n")
+        time.sleep(0.5)
+
+
+juego_del_dado()
