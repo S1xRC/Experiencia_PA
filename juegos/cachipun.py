9d390d6a9d2894e860eb5e456c3fe0268df4b844
S1xRC <163365038+S1xRC@users.noreply.github.com>
2024-03-14 13:50:11 +0000

    piedra

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,17 @@
+import random
+
 def cachipun():
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
-    pass
+    elejido_comp=random.randint(0,2)
+    Eleccion=str.lower(input('Elije tu movimiento, Papel, Piedra o Tijera: '))
+    Posibles=['piedra','papel','tijera']
+    print('La computadora ha elejido:', Posibles[elejido_comp])
+    for x in range(2):
+        if Eleccion==Posibles[x]:
+            Eleccion=x
+    if Eleccion==elejido_comp:
+        return(print('Has empatado'))
+    elif Eleccion==elejido_comp+1:
+        return(print('Has ganado'))
+    else:
+        return(print('Has perdido'))
+cachipun()
